d92e3bb2a06b14782bfb19b1404f42e79d50725e
huda_aeda <44691789+hudaaeda10@users.noreply.github.com>
2021-04-08 20:44:34 +0700

    Pertemuan #3 - Bootstrap Table 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react'
 import NavbarComponent from './components/NavbarComponent'
 import JumbotronComponent from './components/JumbotronComponent'
+import TableComponent from './components/TableComponent'
 
 export default class App extends Component {
   state = {
-    title: 'Code Lyoko Academy'
+    title: 'Code Lyoko Academy',
+    users: [
+      {
+        id: 1,
+        nama: 'Kana Hanazawa',
+        alamat: 'Garut',
+        umur: 23,
+        nohp: "02123452221"
+      },
+      {
+        id: 2,
+        nama: 'Horimiya',
+        alamat: 'Jepara',
+        umur: 21,
+        nohp: "0212345213"
+      },
+      {
+        id: 1,
+        nama: 'Kana Hanazawa',
+        alamat: 'Garut',
+        umur: 23,
+        nohp: "02182927287"
+      }
+    ]
   }
   render() {
     return (
       <div>
         <NavbarComponent/>
         <JumbotronComponent title={this.state.title}/>
+        <TableComponent users={this.state.users} />
       </div>
     )
   }
---
+++ src/components/TableComponent.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import BootstrapTable from 'react-bootstrap-table-next';
+import { Container, Button } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfo, faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+    const columns = [{
+        dataField: 'id',
+        text: 'ID',
+        headerStyle: () => {
+            return {width: "5%"};
+        },
+    }, {
+        dataField: 'nama',
+        text: 'Nama'
+    }, {
+        dataField: 'alamat',
+        text: 'Alamat'
+    },
+    {
+        dataField: "link",
+        text: "Action",
+        formatter: (rowContent, row) => {
+            return (
+                <div>
+                    <Button color="dark" className="mr-2">
+                        <FontAwesomeIcon icon={faInfo} />Detail
+                    </Button>
+
+                    <Button color="dark" className="mr-2">
+                        <FontAwesomeIcon icon={faEdit} />Edit
+                    </Button>
+
+                    <Button color="dark" className="mr-2">
+                        <FontAwesomeIcon icon={faTrash} />Delete
+                    </Button>
+                </div>
+            );
+        }
+    }
+];
+const TableComponent = (props) => {
+    return (
+        <div>
+            <Container>
+                <BootstrapTable keyField='id' data={ props.users } columns={ columns } />   
+            </Container>
+        </div>
+    )
+}
+
+export default TableComponent
